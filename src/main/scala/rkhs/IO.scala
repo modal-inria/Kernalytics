ddfc8c6324eda06adee53518e331474c1c1fe852
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-01-25 14:13:22 +0100

    IO simplified by uncurrying

--- src/main/scala/rkhs/IO.scala
+++ src/main/scala/rkhs/IO.scala
@@ -8,62 +8,46 @@ import various.TypeDef._
 /**
  * Parsing for instanciation of kernels.
  */
-object IO {
-  class IOKernel(
-      val name: String,
-      val paramConv: (String, KerEval.DataRoot) => Try[(Index, Index) => Real])
-   
-   val Linear = new IOKernel(
-       "Linear",
-       (paramStr, dv) => {
-         dv match {
-           case KerEval.DenseVectorReal(data) => {
-             Success(KerEval.generateKerEval(
-            		 data,
-            		 Kernel.InnerProduct.linear(
-            				 _: Real,
-            				 _: Real,
-            				 Algebra.R.InnerProductSpace),
-            		 true))
-           }
-           
-           case _ => Failure(new Exception("Linear kernel is not available for " + dv.typeName))
-         }
+object IO {  
+   def generateKernel(nameStr: String, paramStr: String, data: KerEval.DataRoot): Try[(Index, Index) => Real] = data match {
+     case KerEval.DenseVectorReal(data) if nameStr == "Linear" => {
+      	 Success(KerEval.generateKerEval(
+      			 data,
+      			 Kernel.InnerProduct.linear(
+      					 _: Real,
+      					 _: Real,
+      					 Algebra.R.InnerProductSpace),
+      			 true))
+     }
+     
+     case KerEval.DenseVectorReal(data) if nameStr == "Gaussian" => {
+       Try(paramStr.toDouble)
+       .map(sd => {
+      	   KerEval.generateKerEval(
+      					   data,
+      					   Kernel.InnerProduct.gaussian(
+      							   _: Real,
+      							   _: Real,
+      							   Algebra.R.InnerProductSpace,
+      							   sd),
+      					   true)
        })
-      
-   val Gaussian = new IOKernel(
-       "Gaussian",
-       (paramStr, dv) => {
-         Try(paramStr.toDouble)
-         .flatMap(sd => {
-           dv match {
-             case KerEval.DenseVectorReal(data) => 
-               Success(
-                   KerEval.generateKerEval(
-                		   data,
-                		   Kernel.InnerProduct.gaussian(
-                				   _: Real,
-                				   _: Real,
-                				   Algebra.R.InnerProductSpace,
-                				   sd),
-                		   true))
-              		 
-             case KerEval.DenseVectorMatrixReal(data) =>
-               Success(
-            		   KerEval.generateKerEval(
-            				   data,
-            				   Kernel.Metric.gaussian(
-            						   _: DenseMatrix[Real],
-            						   _: DenseMatrix[Real],
-            						   Algebra.DenseMatrixReal.MetricSpace,
-            						   sd),
-            				   true))
-            				   
-             case _ => Failure(new Exception("Gaussian kernel is not available for " + dv.typeName))
-           }
-         })
-       })
-  
-   /** To be available, a Kernel must be included in this Array. */
-   val registeredKernels: Array[IOKernel] = Array(Linear, Gaussian)
+     }
+       
+     case KerEval.DenseVectorMatrixReal(data) if nameStr == "Gaussian" => {
+      	 Try(paramStr.toDouble)
+      	 .map(sd => {
+      		 KerEval.generateKerEval(
+      				 data,
+      				 Kernel.Metric.gaussian(
+      						 _: DenseMatrix[Real],
+      						 _: DenseMatrix[Real],
+      						 Algebra.DenseMatrixReal.MetricSpace,
+      						 sd),
+      				 true)
+      	 })
+     }
+     
+     case _ => Failure(new Exception(nameStr + " kernel is not available for " + data.typeName + "data type."))
+   }
 }
