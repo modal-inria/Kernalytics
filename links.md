180e149a519f55c570d017004d597afbaebe3217
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-03-20 17:50:53 +0100

    other files moved

--- links.md
+++
@@ -1,1 +0,0 @@
-http://www.scalatest.org/user_guide/using_scalatest_with_sbt
--- src/main/scala/various/Sandbox.scala
+++
@@ -1,7 +0,0 @@
-package various
-
-object Sandbox {  
-  def main {
-    (0 to 10).map(i => (i, Math.factorial(i))).foreach(println)
-  }
-}
