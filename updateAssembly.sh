c3ab381add9bb4a522db0fc45328e238adc1512b
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-05-28 15:50:45 +0200

    updateAssembly.sh builds and installs the R package

--- updateAssembly.sh
+++ updateAssembly.sh
@@ -1,2 +1,3 @@
 cd kernalytics; sbt assembly; cd ..
 cp kernalytics/target/scala-2.12/Kernalytics-assembly-1.0.jar kernalyzr/inst/java/scala-2.12
+R CMD INSTALL --no-multiarch --with-keep.source kernalyzr
