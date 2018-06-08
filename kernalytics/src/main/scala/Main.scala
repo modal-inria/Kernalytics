e92ee8965e72f2fa233bedc008f24902a37a97e5
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-06-08 15:06:12 +0200

    svm learn / prediction validated

--- kernalytics/src/main/scala/Main.scala
+++ kernalytics/src/main/scala/Main.scala
@@ -16,7 +16,8 @@ object Main extends App {
 
   //  svm.SimpleCase2D.main
   //  algo.svm.examples.TestSandBox.testOptim
-//  algo.svm.examples.SimpleCase2D.writeAll
+  algo.svm.examples.SimpleCase2D.writeAll
   exec.Examples.svmLearn
   exec.Examples.svmPredict
+  algo.svm.examples.SimpleCase2D.checkPrediction
 }
--- kernalytics/src/main/scala/algo/svm/examples/SimpleCase2D.scala
+++ kernalytics/src/main/scala/algo/svm/examples/SimpleCase2D.scala
@@ -4,6 +4,7 @@ import breeze.linalg._
 import breeze.stats.distributions._
 import java.io.File
 import org.apache.commons.io.FileUtils
+import scala.io.Source
 
 import various.Def
 import various.TypeDef._
@@ -16,6 +17,10 @@ object SimpleCase2D {
   val rootFolder = "data/exec/svm"
   val varName = "v1"
   val varType = "VectorReal,2"
+  val learnYFile = rootFolder + Def.folderSep + "learnY.csv"
+  val predictYFile = rootFolder + Def.folderSep + "predictY.csv"
+  val predictYExpectedFile = rootFolder + Def.folderSep + "predictYExpected.csv"
+  val predictConfusionFile = rootFolder + Def.folderSep + "predictConfusion.csv"
 
   def writeAll {
     writeData
@@ -28,7 +33,7 @@ object SimpleCase2D {
     val C: Real = 1000 // large value to penalize non compliance with margins
 
     val xSample = new Uniform(0.0, 2.0 * math.Pi)
-    val ySample = new Uniform(0.0, 1.0)
+    val ySample = new Uniform(-1.0, 1.0)
 
     val dataLearn = Array.fill[Array[Real]](nPoints)(Array[Real](xSample.sample, ySample.sample))
     val yLearn = dataLearn.map(computeY)
@@ -38,7 +43,7 @@ object SimpleCase2D {
     FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "learnData.csv"), xLearnStr, "UTF-8")
 
     val yLearnStr = yLearn.mkString(Def.eol)
-    FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "learnY.csv"), yLearnStr, "UTF-8")
+    FileUtils.writeStringToFile(new File(learnYFile), yLearnStr, "UTF-8")
 
     val dataPredict = Array.fill[Array[Real]](nPoints)(Array[Real](xSample.sample, ySample.sample))
     val yPredict = dataPredict.map(computeY)
@@ -48,7 +53,7 @@ object SimpleCase2D {
     FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "predictData.csv"), xPredictStr, "UTF-8")
 
     val yPredictStr = yPredict.mkString(Def.eol)
-    FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "predictYExpected.csv"), yPredictStr, "UTF-8")
+    FileUtils.writeStringToFile(new File(predictYExpectedFile), yPredictStr, "UTF-8")
   }
 
   def computeY(point: Array[Real]): Real =
@@ -64,7 +69,7 @@ object SimpleCase2D {
     FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "algo.csv"), algoStr, "UTF-8")
 
     val desc = Array(
-      Array(varName, "1.0", "Gaussian(10.0)"))
+      Array(varName, "1.0", "Gaussian(1.0)"))
     val descStr = desc.transpose.map(_.mkString(Def.csvSep)).mkString(Def.eol)
     FileUtils.writeStringToFile(new File(rootFolder + Def.folderSep + "desc.csv"), descStr, "UTF-8")
   }
@@ -72,7 +77,31 @@ object SimpleCase2D {
   /**
    * Compute and export confusion matrix.
    */
-  def checkPrediction {
-    ???
+  def checkPrediction = {
+    val res = DenseMatrix.zeros[Real](2, 2)
+
+    val expectedData =
+      Source
+        .fromFile(predictYExpectedFile)
+        .getLines
+        .toArray
+        .map(_.toReal)
+
+    val computedData =
+      Source
+        .fromFile(predictYFile)
+        .getLines
+        .toArray
+        .map(_.toReal)
+
+    val nObs = expectedData.length
+
+    for (i <- 0 to nObs - 1) {
+      val currData = Array(expectedData(i), computedData(i))
+      val indices = currData.map(y => if (y < 0.0) 0 else 1)
+      res(indices(0), indices(1)) += 1
+    }
+
+    csvwrite(new File(predictConfusionFile), res)
   }
 }
