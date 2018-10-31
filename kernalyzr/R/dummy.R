e30001444eb86adab55ef82ce3a3ef6105a64c18
Vincent KUBICKI <vincent.kubicki@inria.fr>
2018-10-31 14:42:40 +0100

    configure script disabled: did not output the complete assembly package

--- kernalyzr/R/dummy.R
+++ kernalyzr/R/dummy.R
@@ -1,7 +1,3 @@
-dummy <- function(rootFolder) {
+rScalaBridgeTest <- function(rootFolder) {
   return(s$exec.Dummy.main(rootFolder))
-}
-
-dummyA <- function(rootFolder) {
-  return(s$bob.Main.main())
 }
