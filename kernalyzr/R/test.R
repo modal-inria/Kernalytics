62b61acf70b01029366b22a1f9e04cdd22e5fc55
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2020-01-21 11:45:20 +0100

    rename

--- kernalyzr/R/test.R
+++ kernalyzr/R/test.R
@@ -1,11 +1,20 @@
-#' Copy the examples in current directory, for easier testing
+# Copy the examples in current directory, for easier testing
+#
+# @author Vincent Kubicki
 copyTest <- function() {
     file.copy(system.file("extdata", "offlinechangepoint", package = "kernalyzr"), ".", recursive = TRUE)
     file.copy(system.file("extdata", "regression", package = "kernalyzr"), ".", recursive = TRUE)
     file.copy(system.file("extdata", "svm", package = "kernalyzr"), ".", recursive = TRUE)
 }
 
+# Run examples
+#
+# @author Vincent Kubicki
+#
+# @export
 runTest <- function() {
+  copyTest()
+
   kernelLearn("offlinechangepoint")
 
   kernelLearn("regression")
