8c70e770c30089881dd855a8bec0bd11fba24253
Quentin Grimonprez <quentin.grimonprez@inria.fr>
2020-01-21 14:10:33 +0100

    change .close in close and remove rscala::

--- kernalyzr/R/onLoad.R
+++ kernalyzr/R/onLoad.R
@@ -1,11 +1,12 @@
+# @author Vincent Kubicki
 .onLoad <- function(libname, pkgname) {
-    if (rscala::scalaConfig()$scalaMajorVersion != "2.12") {
-      rscala::scalaConfig(download="scala")
+    if (scalaConfig()$scalaMajorVersion != "2.12") {
+      scalaConfig(download = "scala")
     }
-    
+
   assign("s", scala(pkgname), envir = parent.env(environment()))
 }
 
 .onUnload <- function(libpath) {
-  .close(s)
+  close(s)
 }
